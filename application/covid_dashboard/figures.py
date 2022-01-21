b124e8e198f5788e86808563afb27d5fb92cd066
kkliu5 <kkliu5@gmail.com>
2022-01-20 14:12:17 -1000

    New code with updated data

--- application/covid_dashboard/figures.py
+++ application/covid_dashboard/figures.py
@@ -8,9 +8,9 @@ from .data import *
 
 # Load DataFrame
 df_case_death_agg = create_case_death_df()
+df_hos_1, df_hos_2, df_hos_3, df_hos_4, df_hos_5, df_hos_6, df_hos_7, df_hos_8, df_hos_9, df_hos_10 = create_hos_df()
 df_test_agg = create_test_df()
 counties, df_vacc_animated, df_vacc_static = create_vacc_df()
-df_hos_1, df_hos_2, df_hos_3, df_hos_4, df_hos_5, df_hos_6, df_hos_7, df_hos_8, df_hos_9, df_hos_10 = create_hos_df()
 
 # Graphing Functions
 def customlegend(fig, new_legend):
@@ -21,13 +21,12 @@ def customlegend(fig, new_legend):
     return(fig)
 
 def fig_format(fig):
-    #fig.update_xaxes(rangeslider=dict(visible=True))
+#     fig.update_xaxes(rangeslider=dict(visible=True))
     fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)',
                        'paper_bgcolor': 'rgba(0,0,0,0)'})
     fig.update_layout(font_color= 'white', xaxis_title='Date', yaxis_title='Amount', legend_title_text='Variable')
     fig.update_xaxes(showgrid=False)
     fig.update_yaxes(showgrid=False)
-    fig.update_layout(title={'y':0.85,'x':0.5,'xanchor': 'center','yanchor': 'top'})
     return(fig)
 
 #Figures/Graphs
@@ -40,7 +39,7 @@ case_fig = customlegend(fig = case_fig,
                                       '7d_ra_case': '7 Day Rolling Average',
                                       '30d_ra_case': '30 Day Rolling Average'})
 fig_format(case_fig)
-case_fig.show()
+
 
 """Daily Deaths"""
 death_fig = px.line(df_case_death_agg, x='submission_date', y=['new_death','7d_ra_death','30d_ra_death'],
@@ -51,7 +50,6 @@ death_fig = customlegend(fig = death_fig,
                                       '7d_ra_death': '7 Day Rolling Average',
                                       '30d_ra_death': '30 Day Rolling Average'})
 fig_format(death_fig)
-death_fig.show()
 
 """Daily Tests"""
 test_fig = px.bar(df_test_agg, x='date', y=['tot_test_daily'],
@@ -60,7 +58,59 @@ test_fig = px.bar(df_test_agg, x='date', y=['tot_test_daily'],
 test_fig = customlegend(fig = test_fig,
                         new_legend = {'tot_test_daily':'Daily Test'})
 fig_format(test_fig)
-test_fig.show()
+
+"""Case Status"""
+hos_case_fig = px.bar(df_hos_1, 
+             x='current_status', 
+             y='count_cdc_case_earliest_dt',
+             labels={"current_status": "Current Status",  "count_cdc_case_earliest_dt": "Count of Cases"},
+             title=('Case Status'))
+fig_format(hos_case_fig)
+"""Cases by Sex"""
+hos_sex_fig = px.bar(df_hos_2, x='sex', y='count_cdc_case_earliest_dt',
+            labels={"sex": "Sex",  "count_cdc_case_earliest_dt": "Count of Cases"},
+            title=('Cases by Sex'))
+fig_format(hos_sex_fig)
+"""Cases by Age Group"""
+hos_age_fig = px.bar(df_hos_3, x='age_group', y='count_cdc_case_earliest_dt',
+            labels={"age_group": "Age Group",  "count_cdc_case_earliest_dt": "Count of Cases"},
+            title=('Cases by Age Group'))
+fig_format(hos_age_fig)
+"""Cases by Race"""
+hos_race_fig = px.bar(df_hos_4, x='race_ethnicity_combined', y='count_cdc_case_earliest_dt',
+            labels={"race_ethnicity_combined": "Race",  "count_cdc_case_earliest_dt": "Count of Cases"},
+            title=('Cases by Race'))
+fig_format(hos_race_fig)
+"""Case by Hospitalzation"""
+hos_hos_fig = px.bar(df_hos_5, x='hosp_yn', y='count_cdc_case_earliest_dt',
+            labels={"hosp_yn": "Hospitalized",  "count_cdc_case_earliest_dt": "Count of Cases"},
+            title=('Cases by Hospitalzation Status'))
+fig_format(hos_hos_fig)
+""" Cases by ICU"""
+hos_icu_fig = px.bar(df_hos_6, x='icu_yn', y='count_cdc_case_earliest_dt',
+            labels={"icu_yn": "ICU Status",  "count_cdc_case_earliest_dt": "Count of Cases"},
+            title=('Cases by ICU Status'))
+fig_format(hos_icu_fig)
+"""Cases by Death Status"""
+hos_death_fig = px.bar(df_hos_7, x='death_yn', y='count_cdc_case_earliest_dt',
+            labels={"death_yn": "Death Status",  "count_cdc_case_earliest_dt": "Count of Cases"},
+            title=('Cases by Death Status'))
+fig_format(hos_death_fig)
+"""Cases with Medical Status"""
+hos_med_fig = px.bar(df_hos_8, x='medcond_yn', y='count_cdc_case_earliest_dt',
+            labels={"medcond_yn": "Medical Status",  "count_cdc_case_earliest_dt": "Count of Cases"},
+            title=('Cases by Presence of Underlying Comorbidity or Disease'))
+fig_format(hos_med_fig)
+"""ICU Cases Over Time"""
+hos_icu_ts_fig = px.line(df_hos_9, x='cdc_case_earliest_dt', y='count_cdc_case_earliest_dt',
+            labels={"cdc_case_earliest_dt": "Date",  "count_cdc_case_earliest_dt": "Count of Cases"},
+            title=('ICU Cases over Time'))
+fig_format(hos_icu_ts_fig)
+"""Hospitalized Cases Over Time"""
+hos_hos_ts_fig = px.line(df_hos_10, x='cdc_case_earliest_dt', y='count_cdc_case_earliest_dt',
+            labels={"cdc_case_earliest_dt": "Date",  "count_cdc_case_earliest_dt": "Count of Cases"},
+            title=('Hospitalized Cases over Time'))
+fig_format(hos_hos_ts_fig)
 
 """Animated Vaccine Map"""
 #creating map that displays percent vaccinated by county over time
@@ -76,7 +126,7 @@ animated_map = px.choropleth_mapbox(df_vacc_animated, geojson=counties, location
                                   'fips':'FIPS Code'}
                           )
 animated_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-animated_map.show()
+
 
 #Static Vaccine Map
 #creating map that displays percent vaccinated by county currently
@@ -91,56 +141,3 @@ current_map = px.choropleth_mapbox(df_vacc_static, geojson=counties, locations='
                                   'fips':'FIPS Code'}
                           )
 animated_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-animated_map.show()
-
-"""Case Status"""
-hos_case_fig = px.bar(df_hos_1, 
-             x='current_status', 
-             y='count_cdc_case_earliest_dt',
-             labels={"current_status": "Current Status",  "count_cdc_case_earliest_dt": "Count of Cases"},
-             title=('Cases'))
-"""Cases by Sex"""
-hos_sex_fig = px.bar(df_hos_2, x='sex', y='count_cdc_case_earliest_dt',
-            labels={"sex": "Sex",  "count_cdc_case_earliest_dt": "Count of Cases"},
-            title=('Cases by Sex'))
-"""Cases by Age Group"""
-hos_age_fig = px.bar(df_hos_3, x='age_group', y='count_cdc_case_earliest_dt',
-            labels={"age_group": "Age Group",  "count_cdc_case_earliest_dt": "Count of Cases"},
-            title=('Cases by Age Group')
-            )
-"""Cases by Race"""
-hos_race_fig = px.bar(df_hos_4, x='race_ethnicity_combined', y='count_cdc_case_earliest_dt',
-            labels={"race_ethnicity_combined": "Race",  "count_cdc_case_earliest_dt": "Count of Cases"},
-            title=('Cases by Race')
-            )
-"""Case by Hospitalzation"""
-hos_hos_fig = px.bar(df_hos_5, x='hosp_yn', y='count_cdc_case_earliest_dt',
-            labels={"hosp_yn": "Hospitalized",  "count_cdc_case_earliest_dt": "Count of Cases"},
-            title=('Cases by Hospitalzation Status')
-            )
-""" Cases by ICU"""
-hos_icu_fig = px.bar(df_hos_6, x='icu_yn', y='count_cdc_case_earliest_dt',
-            labels={"icu_yn": "ICU Status",  "count_cdc_case_earliest_dt": "Count of Cases"},
-            title=('Cases by ICU Status')
-            )
-"""Cases by Death Status"""
-hos_death_fig = px.bar(df_hos_7, x='death_yn', y='count_cdc_case_earliest_dt',
-            labels={"death_yn": "Death Status",  "count_cdc_case_earliest_dt": "Count of Cases"},
-            title=('Cases by Death Status')
-            )
-"""Cases with Medical Status"""
-hos_med_fig = px.bar(df_hos_8, x='medcond_yn', y='count_cdc_case_earliest_dt',
-            labels={"medcond_yn": "Medical Status",  "count_cdc_case_earliest_dt": "Count of Cases"},
-            title=('Cases by Presence of Underlying Comorbidity or Disease')
-            )
-"""ICU Cases Over Time"""
-hos_icu_ts_fig = px.line(df_hos_9, x='cdc_case_earliest_dt', y='count_cdc_case_earliest_dt',
-            labels={"cdc_case_earliest_dt": "Date",  "count_cdc_case_earliest_dt": "Count of Cases"},
-            title=('ICU Cases over Time')
-            )
-"""Hospitalized Cases Over Time"""
-hos_hos_ts_fig = px.line(df_hos_10, x='cdc_case_earliest_dt', y='count_cdc_case_earliest_dt',
-            labels={"cdc_case_earliest_dt": "Date",  "count_cdc_case_earliest_dt": "Count of Cases"},
-            title=('Hospitalized Cases over Time')
-            )
-              
